69871b06d4c70378bdbfbebd23695a885ae6e3dd
OhadR-Cellebrite-PC <ohad.redlich@cellebrite.com>
2019-09-24 14:10:33 +0300

    handle discoverExtraction request

--- src/main/webapp/createCases.js
+++ src/main/webapp/createCases.js
@@ -1,7 +1,8 @@
 var SERVER_ADDRESS = 'http://localhost:8080/rest-api';
 var LOGIN_URL = SERVER_ADDRESS + '/login';
-var CREATE_CASE = SERVER_ADDRESS + '/case/';
+var CASE = SERVER_ADDRESS + '/case';
 var ASSIGN_USER_TO_CASE = SERVER_ADDRESS + '/privilege';
+var DISCO_EXTRACTION = CASE + '/discoverExtractions/';
 
 $(document).ready(function() {
 	$("#submit").click(function(){
@@ -9,9 +10,29 @@ $(document).ready(function() {
 	});
 });
 
+var discoverExtractionRequestData = 
+{
+	"path": "D:/evidences/Old Trafford Bombing/Extractions/Connor.ufdr",
+	"fileTypes": {
+		"zip": true,
+		"dd": true,
+		"txt": true,
+		"001": true,
+		"img": true,
+		"ufd": true,
+		"tar": true,
+		"xml": true,
+		"csv": true,
+		"e01": true,
+		"ufdr": true,
+		"xls": true
+	},
+	"caseId": null
+}
+
 var createCaseRequestData = 
 {
-	"entities": [
+	entities: [
     {
       "type": "case",
       "configuration": {
@@ -24,10 +45,10 @@ var createCaseRequestData =
         "paths": [
           "C:\\Program Files\\Cellebrite Mobile Synchronization\\UFED Analytics Server\\DataDir\\Extractions"
         ],
-        "mediaCategories": [
+        mediaCategories: [
           "Face"
         ],
-        "caseCreationFileTypes": {
+        caseCreationFileTypes: {
           "dd": true,
           "zip": true,
           "txt": true,
@@ -43,10 +64,29 @@ var createCaseRequestData =
         },
         "watchlists": null,
         "adhoc-watchlists": null,
-        "path": []
+        "path": [
+            "D:/evidences/Old Trafford Bombing/Extractions/Connor.ufdr"
+        ]
       },
-      "extractions": [],
-      "extractionsOwners": {},
+      "extractions": [
+          {
+              "type": "extraction",
+              "ownerId": "b894b927-5e4a-48f3-8c7f-bcde89b52bf9",
+              "ufd": "D:\\evidences\\Old Trafford Bombing\\Extractions\\Connor.ufdr",
+              "category": "Mobile",
+              "mainPath": "D:/evidences/Old Trafford Bombing/Extractions/Connor.ufdr"
+            }
+      ],
+      "extractionsOwners": {
+          "b894b927-5e4a-48f3-8c7f-bcde89b52bf9": {
+              "id": "b894b927-5e4a-48f3-8c7f-bcde89b52bf9",
+              "type": "extractionOwner",
+              "isExpanded": true,
+              "name": "Person 1",
+              "color": "#98a5ab",
+              "keepName": false
+            }
+      },
       "creationDate": "2019-09-15T14:40:46.165Z",
       "status": "0",
       "name": "testApi2",
@@ -103,17 +143,54 @@ function login()
 	});
 }
 
-function createCases()
+function createCasesHandler()
+{
+    var numCasesToCreate = $("#num_cases_to_create").val();
+    
+	for(i=0; i < numCasesToCreate; ++i)
+	{
+		createCaseProcess();
+	}
+}
+
+function createCaseProcess()
+{
+	discoverExtraction();
+}
+
+function discoverExtraction()
+{
+    $.ajax({
+		url: DISCO_EXTRACTION,
+        type: 'POST',
+        data: JSON.stringify(discoverExtractionRequestData),
+        dataType: 'text',    //The type of data that you're expecting back from the server
+        contentType: 'application/json',    //When sending data to the server, use this content type
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function(response, textStatus, jqXHR){
+            var discoverExtractionsResponse = JSON.parse(response);
+           	createCase(discoverExtractionsResponse);
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            alert('error discoverExtraction: ' + textStatus)
+        }
+    });
+}
+
+
+function createCase(discoverExtractionsResponse)
 {
 	let caseName = Math.random().toString(36).substring(2, 7);
+	var ownerId = discoverExtractionsResponse.extractions[0].ownerId;
 
-    var distanceKM = $("#distance").val();
-    
     createCaseRequestData.entities[0].name = 
     createCaseRequestData.entities[0].number = caseName;
+    createCaseRequestData.entities[0].extractions.ownerId = ownerId; 
 
     $.ajax({
-		url: CREATE_CASE,
+		url: CASE,
         type: 'POST',
         data: JSON.stringify(createCaseRequestData),
         dataType: 'text',    //The type of data that you're expecting back from the server
